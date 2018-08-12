d85026e09080cfab350a510e02011e2dc85ebd42
jrpereirajr <ze.sjrp@gmail.com>
2018-08-12 13:04:16 -0300

    Update chain-with-reduce.ts

--- promise/chain-with-reduce.ts
+++ promise/chain-with-reduce.ts
@@ -3,7 +3,7 @@ promises.reduce((chain: Promise<Cliente[]>, promise: Promise<Cliente>) => {
   return chain.then<Cliente[]>(clientes => new Promise((resolve, reject) => {
     promise.then(cliente => {
       // consome um cliente e o adiciona no array de clientes
-      clientes.push(cliente);
+      clientes.push(_.cloneDeep(cliente)); // necessário clonar o objeto para evitar problemas com array de referências
       resolve(clientes);
     })
     .catch(err => reject(err));
